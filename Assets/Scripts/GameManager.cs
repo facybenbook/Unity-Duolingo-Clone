efdebe69d5946ab5aa46925f21f89b2f8f4e9832
ddayin <ddayin@hotmail.com>
2022-03-02 21:07:43 +0900

    second

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 1;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update() called!");
+        
     }
 }
