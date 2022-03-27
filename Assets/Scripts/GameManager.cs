f708d15790392818025f9b4be107a95fd9443171
ddayin <ddayin@hotmail.com>
2022-03-27 13:16:37 +0900

    긴 주석 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+  게임 관리자
+  블라블라블라블라
+*/
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
